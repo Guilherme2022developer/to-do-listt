e1949f2fa71c7d4142ac9e64871dbcd1f013a617
Guilherme <guilhermealves4548@gmail.com>
2022-04-30 19:45:46 -0300

    correção

--- src/routes/checkilist.js
+++ src/routes/checkilist.js
@@ -7,19 +7,25 @@ router.get('/', (req, res) => {
     res.send();
 })
 
+
 router.post('/', (req, res) => {
     console.log(req.body);
     res.status(200).send(req.body);
 })
+
+
 router.get('/:id',(req, res) => {
     console.log(req.body);
     res.send(`ID: ${req.params.id}`);
 })
 
+
 router.put('/:id', (req, res) => {
     console.log(req.body);
     res.send(`PUT ID: ${req.params.id}`);
 })
+
+
 router.delete('/:id', (req, res) => {
     console.log(req.body);
     res.send(`DELETE ID: ${req.params.id}`);
