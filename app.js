bbae4103f37a967ac4486d1fae1b53838d7dc8ca
Guilherme <guilhermealves4548@gmail.com>
2022-05-03 00:14:58 -0300

    Upnewprojec

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const checkiListRouter = require('./src/routes/checkilist');
+const path = require('path');
+
+const checkListRouter = require('./src/routes/checklist');
+const rootRouter = require('./src/routes/index');
+
+const methodOverride = require('method-override');
+
+require('./config/database');
+
+
+
 
 
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
-app.use('/checklists',checkiListRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname,'src/views'));
+app.set('view engine', 'ejs');
 
-
+app.use('/', rootRouter);
+app.use('/checklists', checkListRouter);
 
 
 // const log = (req, res, next) => {
